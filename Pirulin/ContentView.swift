4d6314b702a36a1427b50dee12def35563830935
Jesus Luna <jes24.lu@gmail.com>
2026-05-08 11:31:29 -0600

    Initial Commit

---
+++ Pirulin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pirulin
+//
+//  Created by Jesús Luna  on 08/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pirulin/PirulinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PirulinApp.swift
+//  Pirulin
+//
+//  Created by Jesús Luna  on 08/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PirulinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
